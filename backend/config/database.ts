a8ad911ac855bdecd97b10e3aec979705e178046
Sasini Tennakoon <tennakoonsasi5@gmail.com>
2026-05-09 01:18:55 +0530

    Fix SSL config for production DB

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -12,9 +12,9 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: {
-      rejectUnauthorized: false,
-    },
+        ssl: env.bool('DATABASE_SSL', false)
+  ? { rejectUnauthorized: false }
+  : false,
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
