73715b9b89e3c976eb068e1fd2e3824396003789
Sasini Tennakoon <tennakoonsasi5@gmail.com>
2026-05-09 23:16:08 +0530

    removed local host

--- frontend/src/lib/api.tsx
+++ frontend/src/lib/api.tsx
@@ -1,47 +1,41 @@
+const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
+
 export async function getFeaturedProjects() {
   const res = await fetch(
-    "http://localhost:1337/api/projects?filters[featured][$eq]=true&populate=*",
+    `${API_URL}/api/projects?filters[featured][$eq]=true&populate=*`,
     { cache: "no-store" }
   );
-
   return res.json();
 }
 
 export async function getTestimonials() {
   const res = await fetch(
-    "http://localhost:1337/api/testimonials?populate=*"
+    `${API_URL}/api/testimonials?populate=*`
   );
-
   if (!res.ok) throw new Error("Failed to fetch testimonials");
-
   return res.json();
 }
 
 export async function getProjects() {
-  const res = await fetch("http://localhost:1337/api/projects?populate=*");
+  const res = await fetch(
+    `${API_URL}/api/projects?populate=*`
+  );
   return res.json();
 }
 
-const API_URL = "http://localhost:1337/api";
-
 export async function getBlogs() {
   const res = await fetch(
-    "http://localhost:1337/api/blogs?populate=*",
-    {
-      cache: "no-store",
-    }
+    `${API_URL}/api/blogs?populate=*`,
+    { cache: "no-store" }
   );
-
   return res.json();
 }
 
 export async function getBlogBySlug(slug: string) {
   if (!slug) return null;
-
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/blogs?populate=*`,
+    `${API_URL}/api/blogs?filters[slug][$eq]=${slug}&populate=*`,
     { cache: "no-store" }
   );
-
   return res.json();
 }
