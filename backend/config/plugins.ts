fe50392f46cbded2f11799be350339e06755c44b
Sasini Tennakoon <tennakoonsasi5@gmail.com>
2026-05-10 20:52:25 +0530

    cloudinary connect

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -7,6 +7,20 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
   cloud: {
     enabled: env.bool('STRAPI_PLUGIN_CLOUD_ENABLED', false),
   },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
 });
 
 export default config;
